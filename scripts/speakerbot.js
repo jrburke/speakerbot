8631f11b2881f59c13c5fb73573bc82bad637c1d
jrburke <jrburke@gmail.com>
2016-02-22 11:52:41 -0800

    Put in comment to avoid using deprecated documentation syntax warning from hubot

--- scripts/speakerbot.js
+++ scripts/speakerbot.js
@@ -1,3 +1,7 @@
+// Description:
+//   A speakerbot. This comment is like this to avoid hubot parsing for
+//   CoffeeScript comments, emitting "using deprecated documentation syntax"
+//   warning during startup.
 module.exports = function(robot) {
   robot.hear(/I like pie/i, function (res) {
     res.emote('makes a freshly baked pie');
